9dc293a3577ffea1a27025ee5ea6dd1796157247
Titus <handsomekhalo@gmail.com>
2025-07-09 16:13:29 +0200

    fixed base url to host url

--- system_management/views.py
+++ system_management/views.py
@@ -78,8 +78,7 @@ def set_csrf_token(request):
 # View that redirects to Next.js
 def login_view(request):
     # return redirect("http://localhost:3000/")  # Next.js is running here
-    return redirect('http://56.228.24.233:3000/')  # or your real domain
-
+    return redirect('http://52.14.111.23:3000/')  # or your real domain
 
 
 
