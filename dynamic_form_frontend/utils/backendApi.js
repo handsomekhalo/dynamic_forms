27ec25d07a3492820a07691595e405cca47283a1
Titus <handsomekhalo@gmail.com>
2025-07-26 12:53:42 +0200

    changed instance

--- dynamic_form_frontend/utils/backendApi.js
+++ dynamic_form_frontend/utils/backendApi.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 // Create an Axios instance with a predefined configuration
 const backendApi = axios.create({
-  baseURL: 'http://52.14.111.23',
+  baseURL: ' https://3.144.218.251',
+ 
   // baseURL: "http://127.0.0.1:8000",
     // 👈 Public IP of your EC2 instance
   withCredentials: true,          // Ensures cookies (like CSRF token) are sent
